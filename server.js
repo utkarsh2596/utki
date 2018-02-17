945726088aa468b38dc00ce18853a9b941abeda2
Utkarsh Shende <utkarshshende2596@gmail.com>
2018-02-17 13:43:35 +0530

    [haha]

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const app = express()
+const PORT=process.env.PORT ||2000
+
+app.use(function(eq,res,next)){
+  if(req.headers['x-fowarded-proto']=='https'){
+    res.redirect('http://'+req.hostname+req.url);
+  }else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
 app.listen(3000, function () {
-  console.log('Website is running on localhost:3000 ')
-})
+  console.log('Website is running on localhost:'+PORT);
+});
